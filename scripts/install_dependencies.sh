e9692c8d4d061c0c0284c66559866174ab137139
mayor_DevSecOps <101404646+mayorpasca32@users.noreply.github.com>
2025-06-13 16:07:50 +0100

    Create install_dependencies.sh

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
+  | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
